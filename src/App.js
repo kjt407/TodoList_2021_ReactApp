5cb79da1d457762104f22f5bcc843e64ae8226b0
kjt407 <kjt40700@gmail.com>
2021-06-20 15:08:35 +0900

    result capture upload

--- src/App.js
+++ src/App.js
@@ -174,9 +174,16 @@ class App extends React.Component {
               }}
             />
             <Button
-              variant="outlined"
+              variant="contained"
+              color="primary"
               startIcon={<SaveIcon />}
-              style={{ float: "right" }}
+              style={{ float: "right",background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+              borderRadius: 3,
+              border: 0,
+              color: 'white',
+              height: 48,
+              padding: '10px 10px',
+              boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)', }}
               onClick={() => this.saveTodoList()}
             >
               Save
